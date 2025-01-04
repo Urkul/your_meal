8d757c612c0626ceff39de2ef911dedcd6382ca0
Urkul <urkul74@gmail.com>
2025-01-04 17:06:30 +0200

    modified:   js/const.js

--- js/const.js
+++ js/const.js
@@ -1,2 +1,3 @@
-export const API_URL = 'http://localhost:3024';
+// export const API_URL = 'http://localhost:3024';
+export const API_URL = 'https://green-muddy-buckthornpepperberry.glitch.me';
 export const PREFIX_PRODUCT = '/api/product';
